d474fa8649865ca2f0c8f042e9a67b934734f6a5
Kerlen Melo <125043300+kerlenmelo@users.noreply.github.com>
2024-03-15 14:46:14 -0300

    Estrutura de Dados: Lista Circular Dupla
    Correção

---
+++ ListaCircularDupla/src/entities/No.java
@@ -0,0 +1,39 @@
+package entities;
+
+public class No {
+
+    int info;
+    No prox;
+    No ant;
+    
+    public No(int info, No prox, No ant) {
+        this.info = info;
+        this.prox = prox;
+        this.ant = ant;
+    }
+
+    public int getInfo() {
+        return info;
+    }
+
+    public void setInfo(int info) {
+        this.info = info;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+
+    public No getAnt() {
+        return ant;
+    }
+
+    public void setAnt(No ant) {
+        this.ant = ant;
+    }
+
+}
